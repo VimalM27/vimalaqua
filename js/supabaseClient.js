c6085f3ba92257b2159e399d2f8255c42c045fdf
sonalinayak12 <sn949711@gmail.com>
2026-07-30 09:42:18 +0530

    main

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,3 @@
-
 const supabaseUrl = "https://colmyyuwvqijzdeskjmh.supabase.co";
 const supabaseKey = "sb_publishable_59LgUr6efJ4p0mAALpXcnw_omXBSPxF";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
